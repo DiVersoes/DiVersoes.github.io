60424b892f514ce33b2922304ef299eb8b901e93
Toamig <32844574+toamig@users.noreply.github.com>
2026-04-03 23:23:41 +0100

    Update config.js

--- wishlist/config.js
+++ wishlist/config.js
@@ -4,8 +4,8 @@
 
 const WISHLIST_CONFIG = {
   // Free API key from brickset.com/tools/webservices/requestkey
-  bricksetKey: '',
+  bricksetKey: '3-EilS-W4l4-rhoLN',
 
   // Free master key from jsonbin.io → Account → API Keys → Master Key
-  jsonbinKey: '',
+  jsonbinKey: '$2a$10$X8kSdCUqJo2dUV1fjGuzu.sYy0MP//dB52HNecVrETp/WzaOBzNvO',
 };
